f353a3b8276d95abfef9507ecbf73e4b167155ae
amruth-sn <amruth@bu.edu>
2023-08-17 00:41:36 -0400

    init changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,118 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import "./App.css";
 import Image1 from "./assets/tubebyte_cropped.png";
 function App() {
   const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("");
+  const [videoLink, setVideoLink] = useState("");
+  const [songURL, setSongURL] = useState("");
+  const [songTitle, setSongTitle] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
+  // useEffect(() => {
+  //   fetch("/message")
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message));
+  // }, []);
+  // useEffect(() => {
+  //   fetch("/convert")
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message));
+  // }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("/convert", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ videolink: videoLink }),
+    })
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
+      .then((data) => {
+        setMessage(data.message);
+        setSuccess(data.success);
+        setSongURL(data.songURL);
+        setSongTitle(data.songTitle);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  
   return (
     <div>
       <div className="Site">
         <img src={Image1} alt="" />
-        <h1>Welcome!</h1>
+        <h1 className="header">
+          Welcome! Enter a YouTube link to get started.
+        </h1>
         <div className="top">
-          <form action="convert" method="POST" id="form">
+          <form
+            onSubmit={handleSubmit}
+            action="convert"
+            method="POST"
+            id="form"
+          >
             <div>
-              <input type="text" name="videolink" placeholder="Video Link" />
-              <button id="convert-button">Convert!</button>
+              <input
+                className="textbox"
+                type="text"
+                onChange={(e) => setVideoLink(e.target.value)}
+                value={videoLink}
+                name="videolink"
+                placeholder=" Video Link"
+                autoComplete="off"
+              ></input>
+              <button className="button" id="convert-button">
+                Convert!
+              </button>
             </div>
           </form>
+          <br></br>
+          <div>
+            <h1 className="mid-header">
+              Click the download link when it appears.{" "}
+            </h1>
+          </div>
         </div>
-        {/* <div className="bottom">
-          {success && typeof success !== undefined ? (
-            <div className="if_works">
-              <p>{songTitle}</p>
-              <a href={songURL} target="_blank" rel="noopener noreferrer">
-                <button id="download-button">Download</button>
-              </a>
-            </div>
-          ) : !success ? (
-              <div className="error"><p>{error_msg}</p></div>
-          ) : (
-            <div></div>
-          )}
-        </div> */}
-      </div>
-      <div className="App">
-        <center>
-          <h1>{message}</h1>
-        </center>
+        <div className="App">
+          <center>
+            {!success && (
+              <h1 className="mid-header">
+                <p>{message}</p>
+              </h1>
+            )}
+            {success && (
+              <h1 className="bottom-header">
+                <p>Conversion Result:</p>
+                <p>{songTitle}</p>
+                <a href={songURL} target="_blank" rel="noopener noreferrer">
+                  <button className="button" id="download-button">
+                    Download
+                  </button>
+                </a>
+                <br></br>
+                <div className="extras">
+                  <p>
+                    After clicking the download link, you will be taken to
+                    another page. Please ignore the ads and click the dark blue
+                    'Download' button. The site is 100% safe and you will not
+                    encounter any malware. Let me know if you have any
+                    questions!   
+                    <a className="link"
+                      href="https://www.amruthn.com/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                       Contact me
+                    </a>
+                  </p>
+                </div>
+              </h1>
+            )}
+          </center>
+        </div>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const path = require('path')
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
 app.use(cors());
 app.use(express.json());
-
-app.get("/message", (req, res) => {
-  res.send("Hello World");
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.urlencoded({ extended: true }));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.post("/convert", async (req, res) => {
-  const videolink = req.body.videolink;
-  console.log(videolink);
-  res.json({ success: true, message: "Conversion successful" });
+  let videolink = req.body.videolink;
+  if (videolink === "" || videolink === null || videolink === undefined || !videolink.includes("youtube.com")) {
+    res.json({ success: false, message : "Please enter valid video link "})
+  }
+  else {
+    const parts = videolink.split("=");
+    videolink = parts[1];
+    // console.log("Received videolink in backend:", videolink);
+    // res.json({ success: true, message: `Conversion successful --> ${videolink}` });
+    const fetchAPI = await fetch(
+      `https://youtube-mp3-download1.p.rapidapi.com/dl?id=${videolink}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": process.env.API_HOST
+      }
+    });
+
+    const fetchResponse = await fetchAPI.json();
+
+    if (fetchResponse.status === "ok") {
+      return res.json({ success: true, songTitle: fetchResponse.title, songURL: fetchResponse.link});
+    }
+    else {
+      return res.json({ success: false, message: fetchResponse.msg});
+    }
+  }
 });
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
