40a4271d441ba7a3a728d662002753e0246aca92
amruth-sn <amruth@bu.edu>
2023-08-17 13:44:27 -0400

    changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const path = require('path')
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/build")));
